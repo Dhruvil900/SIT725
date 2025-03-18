66649e6c6904c1247fd1f1e8158cb6f7a737163b
Dhruvil <dhruvilbhanderi@gmail.com>
2025-03-18 12:48:25 +1100

    CHanges for task 2.2P

--- Week2/2.2P/server.js
+++ Week2/2.2P/server.js
@@ -1,5 +1,20 @@
 var express = require("express");
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log("App listening to: " + port));
+app.listen(port, () =>
+  console.log(`App listening to: http://localhost:${port} `)
+);
+app.get("/add", (req, res) => {
+  let num1 = parseFloat(req.query.num1);
+  let num2 = parseFloat(req.query.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.status(400).send({ error: "Invalid numbers" });
+  }
+
+  let sum = num1 + num2;
+  res.send({ result: sum });
+});
